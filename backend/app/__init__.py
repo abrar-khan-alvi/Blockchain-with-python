80063c50ac137d4941e095785c57e4f0f79cb196
abrar-khan-alvi <abrar1khan2@gmail.com>
2024-07-08 11:20:36 +0600

    blockchain with web server set up

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+from backend.blockchain.blockchain import Blockchain
 
 app = Flask(__name__)
+
+blockchain = Blockchain()
+for i in range(3):
+    blockchain.add_block(i)
+
 
 @app.route('/')
 
 def default():
     return f'<h1>Welcome to the blockchain</h1>'
-def test():
-    return 'Test'
+
+@app.route('/blockchain')
+
+def route_blockchain():
+    return jsonify(blockchain.to_json())
 
 app.run()
+
