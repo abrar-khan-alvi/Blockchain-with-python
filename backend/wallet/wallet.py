11a4e10368035d2af960b2dee6a386c3075d3cd4
abrar-khan-alvi <abrar1khan2@gmail.com>
2024-09-14 02:19:08 +0600

    transact init

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -4,8 +4,9 @@ import uuid
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes,serialization
 from cryptography.exceptions import InvalidSignature
+
 
 class Wallet:
     """
@@ -21,6 +22,7 @@ class Wallet:
             default_backend()
         )
         self.public_key = self.private_key.public_key()
+        self.serialize_public_key()
 
     def sign(self, data):
         """
@@ -31,13 +33,31 @@ class Wallet:
             ec.ECDSA(hashes.SHA256())
         )
 
+    def serialize_public_key(self):
+        """
+        Reset the public key to it's serialized version.
+        """
+        self.public_key_bytes = self.public_key.public_bytes(
+            encoding = serialization.Encoding.PEM, 
+            format = serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        # print(f'\nself.public_key_bytes: {self.public_key_bytes}')
+
+        decoded_public_key = self.public_key_bytes.decode('utf-8')
+        # print(f'decoded_public_key : {decoded_public_key}')
+        self.public_key = decoded_public_key
+    
     @staticmethod
     def verify(public_key, data, signature):
         """
         Verify a signature based on the original public key and data.
         """
+        deserialized_public_key = serialization.load_pem_public_key(
+            public_key.encode('utf-8'),
+            default_backend()
+        )
         try:
-            public_key.verify(
+            deserialized_public_key.verify(
                 signature,
                 json.dumps(data).encode('utf-8'),
                 ec.ECDSA(hashes.SHA256())    
